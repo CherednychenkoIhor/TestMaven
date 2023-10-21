5409f90953bc012438da914dd5b899eeafebc405
Ihor <ihor.cherednychenko@nure.ua>
2023-10-21 18:48:01 +0300

    JUnit tests, part II

--- src/test/java/Vector2DTest.java
+++ src/test/java/Vector2DTest.java
@@ -1,29 +1,29 @@
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Vector2DTest {
+    private final double ESP = 1e-9;
+    private static Vector2D v1;
+
+    @BeforeClass
+    public static void createNewVector() {
+        v1 = new Vector2D();
+    }
 
     @Test
     public void newVectorShouldHaveZeroLength() {
-        Vector2D v1 = new Vector2D(); // action
-
         // assertion
-        Assert.assertEquals(0, v1.length(), 1e-9);
+        Assert.assertEquals(0, v1.length(), ESP);
     }
 
     @Test
     public void newVectorShouldHaveZeroX() {
-        Vector2D v1 = new Vector2D();
-
-        Assert.assertEquals(0, v1.getX(), 1e-9);
+        Assert.assertEquals(0, v1.getX(), ESP);
     }
 
     @Test
     public void newVectorShouldHaveZeroY() {
-        Vector2D v1 = new Vector2D();
-
-        Assert.assertEquals(0, v1.getY(), 1e-9);
+        Assert.assertEquals(0, v1.getY(), ESP);
     }
-
-
 }
